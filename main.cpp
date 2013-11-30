5927e9c35f04b09d9a8f628969fc69d1a30b153e
kakadu <kakadu.hafanana@gmail.com>
2013-11-30 18:25:28 +0400

    Fixed main.cpp to use platform specific style rendering

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "stubs.h"
 
 #include <QtGui/QGuiApplication>
+//#include <QApplication>
+#include <QtWidgets/QApplication>
 #include <QtQuick/qquickview.h>
 #include <QtQml/QQmlEngine>
 #include <QtQml/QQmlComponent>
+#include <QtQml/QQmlApplicationEngine>
 
 void doCaml() {
   CAMLparam0();
@@ -18,25 +21,22 @@ void doCaml() {
 
 int main(int argc, char ** argv) {
     caml_main(argv);
-    QGuiApplication app(argc, argv);
+    QApplication app(argc, argv);
 
-    QQmlEngine engine;
-    QQmlComponent component(&engine);
+    QQmlApplicationEngine engine;
 
     QQmlContext *ctxt = engine.rootContext();
     registerContext(QString("rootContext"), ctxt);
     doCaml();
 
-    component.loadUrl(QUrl("qrc:/Root.qml"));
-    QObject *topLevel = component.create(ctxt);
-    if (component.isError()) {
-      qDebug() << component.errors();
-      return 1;
+    engine.load("Root.qml");
+    QList<QObject*> xs = engine.rootObjects();
+    if (xs.length() == 0) {
+        qDebug() << "Can't load QML components. Exit.";
+        return 1;
     }
-    QQuickWindow *window = qobject_cast<QQuickWindow*>(topLevel);
-    // allow using Qt.quit() from QML
-    QObject::connect(&engine,SIGNAL(quit()),&app,SLOT(quit()) );
-	window->setTitle( QObject::tr("QOCamlBrowser: Demo app for lablqt&mocml") );
-    window->showMaximized();
+    QQuickWindow *window = qobject_cast<QQuickWindow*>(xs.at(0));
+    window->showMaximized(); 
     return app.exec();
 }
+
